185fc08ccb0282e1c7266bdbf75a13d30fb4ccaf
ajzal-byte <ajzalaju10@gmail.com>
2024-01-13 05:50:09 +0530

    Prime number

---
+++ Math Algorithms/primeNumber.js
@@ -0,0 +1,19 @@
+// Check if a number is prime or not
+
+function isPrime(num) {
+  if (num < 2){
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(1));
+console.log(isPrime(5));
+console.log(isPrime(4));
+
+// Big O - O(sqrt(n))
