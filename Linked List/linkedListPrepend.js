2e33597a0eb33147b5aed8f8a8876072ee41e313
ajzal-byte <ajzalaju10@gmail.com>
2024-01-15 20:10:08 +0530

    Print after prepending

---
+++ Linked List/linkedListPrepend.js
@@ -0,0 +1,57 @@
+class Node{
+  constructor(value, next = null){
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty(){
+    return this.size == 0;
+  }
+
+  getSize(){
+    return this.size;
+  }
+
+  prepend(value){
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    }else{
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++
+  }
+
+  print(){
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    }else{
+      let curr = this.head;
+      let listValues = '';
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next; 
+      }
+      console.log(listValues);
+    }
+    
+  }
+}
+
+const list = new LinkedList();
+console.log('is List Empty: ', list.isEmpty());
+console.log('Size of List : ', list.getSize());
+list.print();
+list.prepend(10);
+list.print();
+list.prepend(20);
+list.prepend(30);
+list.print();
