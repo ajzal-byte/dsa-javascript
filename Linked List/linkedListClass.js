63c460b9b6b2e14495ae634d9f19dae8cab7a7c4
ajzal-byte <ajzalaju10@gmail.com>
2024-01-15 19:00:32 +0530

    Linked List Class

---
+++ Linked List/linkedListClass.js
@@ -0,0 +1,25 @@
+class Node{
+  constructor(data, next = null){
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty(){
+    return this.size == 0;
+  }
+
+  getSize(){
+    return this.size;
+  }
+}
+
+const list = new LinkedList();
+console.log('is List Empty: ', list.isEmpty());
+console.log('Size of List : ', list.getSize());
