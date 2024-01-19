4b46b904dfef98ebc5334c0f3b321699041f0d4f
ajzal-byte <ajzalaju10@gmail.com>
2024-01-19 18:27:09 +0530

    descending remarked

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -2,7 +2,7 @@ function insertionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let numberToInsert = arr[i];
     let j = i - 1;
-    while (j >= 0 && arr[j] > numberToInsert) {
+    while (j >= 0 && arr[j] > numberToInsert) {  // for descending: while (j >= 0 && arr[j] < numberToInsert)
       arr[j + 1] = arr[j];
       j = j -1;
     }
--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -2,7 +2,7 @@ function selectionSort(arr){
   for(let i =0; i < arr.length -1; i++){
       let minIndex = i;
       for(let j =i+1; j < arr.length; j++){
-          if(arr[j] < arr[minIndex]) minIndex = j;
+          if(arr[j] < arr[minIndex]) minIndex = j;  // for descending: if(arr[j] > arr[minIndex])
       }
       if(minIndex != i){
           [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
