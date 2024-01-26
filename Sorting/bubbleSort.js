3224961db97886d6d824a2d6b7a0b85e1faa1bbc
ajzal-byte <ajzalaju10@gmail.com>
2024-01-26 10:15:56 +0530

    bug fixed

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,8 +1,8 @@
 function bubbleSort(arr){
   let swapped;
   do {
+    swapped = false;
     for (let i = 0; i < arr.length -1; i++) {
-      swapped = false;
       if (arr[i] > arr[i + 1]) {  // for descending order: if (arr[i] < arr[i + 1])
         let temp = arr[i];
         arr[i] = arr[i+1];
@@ -14,7 +14,7 @@ function bubbleSort(arr){
   return arr;
 }
 
-const arr = [10, 1, 4, 20, 5];
+const arr = [10, 4, 8, 2, 0, 7];
 console.log(bubbleSort(arr));
 
 // Big-O = O(n^2)
