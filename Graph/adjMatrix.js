6c26c7115695d2e8a441ff93b8433238e2401e66
ajzal-byte <ajzalaju10@gmail.com>
2024-02-05 12:57:38 +0530

    Adjacency Matrix

---
+++ Graph/adjMatrix.js
@@ -0,0 +1,17 @@
+const matrix = [
+  [0, 1, 0],
+  [1, 0, 1],
+  [0, 1, 0]
+]
+
+console.log("Edge between Node 1 and Node 1:", matrix[0][0]);
+console.log("Edge between Node 1 and Node 2:", matrix[0][1]);
+console.log("Edge between Node 1 and Node 3:", matrix[0][2]);
+
+console.log("Edge between Node 2 and Node 1:", matrix[1][0]);
+console.log("Edge between Node 2 and Node 2:", matrix[1][1]);
+console.log("Edge between Node 2 and Node 3:", matrix[1][2]);
+
+console.log("Edge between Node 3 and Node 1:", matrix[2][0]);
+console.log("Edge between Node 3 and Node 2:", matrix[2][1]);
+console.log("Edge between Node 3 and Node 3:", matrix[2][2]);
