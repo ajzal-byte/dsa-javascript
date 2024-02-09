f597d005cb61dab82fc496bca421a9c89dc73fc1
ajzal-byte <ajzalaju10@gmail.com>
2024-02-09 21:47:20 +0530

    trie remove word implemented

--- Trie/trie.js
+++ Trie/trie.js
@@ -37,6 +37,32 @@ class Trie{
     }
     return currNode.endOfWord;
   }
+
+  removeWord(word, start = this.root){
+    if(!word) return false;
+    let currNode = start;
+    let stack = [];
+
+    for (const letter of word) {
+      if(!currNode.children.has(letter)) return false;
+      currNode = currNode.children.get(letter);
+      if(word[word.length -1] != currNode.value) stack.push(currNode);
+    }
+
+    currNode.endOfWord = false;
+    while (stack.length > 0 && !currNode.endOfWord) {
+      let prevNode = currNode;
+      currNode = stack.pop();
+      if (prevNode.children.size > 0) {
+        break;
+      }
+      currNode.children.delete(prevNode.value);
+    }
+    return true;  
+  }
+  
+
+
 }
 
 
@@ -44,3 +70,26 @@ class Trie{
 // ==========================TEST CASES==========================
 
 const trie = new Trie();
+
+// Insertion
+trie.insert('apple');
+trie.insert('banana');
+trie.insert('app');
+trie.insert('ball');
+
+console.log(trie.hasWord('apple')); // true
+console.log(trie.hasWord('banana')); // true
+console.log(trie.hasWord('app')); // true
+console.log(trie.hasWord('ball')); // true
+console.log(trie.hasWord('orange')); // false
+
+// Removal
+trie.removeWord('apple');
+console.log(trie.hasWord('apple')); // false
+console.log(trie.hasWord('app')); // true
+
+trie.removeWord('orange'); // Should return false as 'orange' is not in the Trie
+console.log(trie.hasWord('orange')); // false
+
+trie.removeWord('ball');
+console.log(trie.hasWord('ball')); // false
